856592c99a469018b1a61946a8341a3b3ea9d790
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-12-19 20:21:01 -0300

    exercise 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    novoProduto = Product(
+        1,
+        "Coca-Cola",
+        "Coca-Cola Company",
+        "2020-01-01", "2020-12-31",
+        "123456789", "em local seco e fresco."
+    )
+    assert novoProduto.id == 1
+    assert novoProduto.nome_do_produto == "Coca-Cola"
+    assert novoProduto.nome_da_empresa == "Coca-Cola Company"
+    assert novoProduto.data_de_fabricacao == "2020-01-01"
+    assert novoProduto.data_de_validade == "2020-12-31"
+    assert novoProduto.numero_de_serie == "123456789"
+    assert novoProduto.instrucoes_de_armazenamento == "em local seco e fresco."
+
