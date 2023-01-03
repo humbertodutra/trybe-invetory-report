95e44be19d58c3c993f5a2b3e34a2554eaa0384b
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-01-03 11:03:50 -0300

    fixing the -istrue-

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -4,14 +4,17 @@ from inventory_report.inventory.product import Product
 def test_relatorio_produto():
     product = Product(
         1,
-        "HulkShirt" "Atletico" "01/12/2022",
+        "Hulk_shirt",
+        "Atletico",
+        "01/12/2020",
         "31/12/2023",
-        "100" "Armazenar em local seco",
+        "100",
+        "Armazenar em local seco",
     )
 
-    assert ("HulkShirt" in product._repr_()).is_true()
-    assert ("Atletico" in product._repr_()).is_true()
-    assert ("2020-01-01" in product._repr_()).is_true()
-    assert ("2024-01-01" in product._repr_()).is_true()
-    assert ("100" in product._repr_()).is_true()
-    assert ("Armazenar em local seco" in product._repr_()).is_true()
+    assert ("Hulk_shirt" in product.__repr__()) is True
+    assert ("Atletico" in product.__repr__()) is True
+    assert ("01/12/2020" in product.__repr__()) is True
+    assert ("31/12/2023" in product.__repr__()) is True
+    assert ("100" in product.__repr__()) is True
+    assert ("Armazenar em local seco" in product.__repr__()) is True
