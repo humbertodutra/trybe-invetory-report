18d3258f27bf9ced36c9cc93b49d8022c9f5a476
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-01-03 10:53:36 -0300

    req 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "HulkShirt" "Atletico" "01/12/2022",
+        "31/12/2023",
+        "100" "Armazenar em local seco",
+    )
+
+    assert ("HulkShirt" in product._repr_()).is_true()
+    assert ("Atletico" in product._repr_()).is_true()
+    assert ("2020-01-01" in product._repr_()).is_true()
+    assert ("2024-01-01" in product._repr_()).is_true()
+    assert ("100" in product._repr_()).is_true()
+    assert ("Armazenar em local seco" in product._repr_()).is_true()
